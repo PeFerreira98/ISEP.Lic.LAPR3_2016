cceb2aadfcb95b168ea62fa8d1691996af7e651c
1140925 <1140925@isep.ipp.pt>
2016-12-13 16:23:22 +0000

    Aircraft.

---
+++ src/main/java/lapr/project/model/Aircraft.java
@@ -0,0 +1,77 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package lapr.project.model;
+
+/**
+ *
+ * @author João
+ */
+public class Aircraft{
+    
+    private AircraftModel model;
+    private int id;
+    private String company;
+    private int numberFirstClass;
+    private int numberNormalClass;
+    private int numberElementsCrew;
+
+    public Aircraft(AircraftModel model, int id, String company, int numberFirstClass, int numberNormalClass, int numberElementsCrew) {
+        this.model = model;
+        this.id = id;
+        this.company = company;
+        this.numberFirstClass = numberFirstClass;
+        this.numberNormalClass = numberNormalClass;
+        this.numberElementsCrew = numberElementsCrew;
+    }
+
+    public AircraftModel getModel() {
+        return model;
+    }
+
+    public void setModel(AircraftModel model) {
+        this.model = model;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getCompany() {
+        return company;
+    }
+
+    public void setCompany(String company) {
+        this.company = company;
+    }
+
+    public int getNumberFirstClass() {
+        return numberFirstClass;
+    }
+
+    public void setNumberFirstClass(int numberFirstClass) {
+        this.numberFirstClass = numberFirstClass;
+    }
+
+    public int getNumberNormalClass() {
+        return numberNormalClass;
+    }
+
+    public void setNumberNormalClass(int numberNormalClass) {
+        this.numberNormalClass = numberNormalClass;
+    }
+
+    public int getNumberElementsCrew() {
+        return numberElementsCrew;
+    }
+
+    public void setNumberElementsCrew(int numberElementsCrew) {
+        this.numberElementsCrew = numberElementsCrew;
+    }
+}
