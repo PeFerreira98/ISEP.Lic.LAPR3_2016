61de99386dee7b2961260422332bf4f0447cec11
PeFerreira98 <1140953@isep.ipp.pt>
2016-12-14 16:41:58 +0000

    Edit AddAircraftModelController to suport Project class

--- src/main/java/lapr/project/controller/AddAircraftModelController.java
+++ src/main/java/lapr/project/controller/AddAircraftModelController.java
@@ -5,23 +5,32 @@
  */
 package lapr.project.controller;
 
-import lapr.project.model.AircraftModel;
+import lapr.project.model.*;
 
 /**
  *
  * @author zero_
  */
 public class AddAircraftModelController {
-    
-    public AircraftModel addAircraftModel (int id, String type, String motorization, 
+
+    private final Project project;
+
+    public AddAircraftModelController(Project project) {
+        this.project = project;
+    }
+
+    public AircraftModel addAircraftModel(int id, String type, String motorization,
             double emptyWeight, double MTOW, double MZFW, double maximumFuelCapacity,
-            double serviceCeiling, double cruiseSpeed, double wingArea, double dragCoeficient, double liftCoeficient){
-        
+            double serviceCeiling, double cruiseSpeed, double wingArea, double dragCoeficient, double liftCoeficient) {
+
         final AircraftModel newAircraftModel = new AircraftModel(id, AircraftModel.Type.valueOf(type), AircraftModel.Motorization.valueOf(motorization), emptyWeight, MTOW, MZFW, maximumFuelCapacity, serviceCeiling, cruiseSpeed, wingArea, dragCoeficient, liftCoeficient);
-        
+
+        if (this.project.addAircraftModel(newAircraftModel) != null) {
 //        final DatabaseModel databaseModel = new DatabaseModel();
 //        databaseModel.addAircraftModel(newAircraftModel);
+        }
 
         return newAircraftModel;
     }
+
 }
