20da6de6d4851c851558caea208426391cf7f9fe
Marcos Dourado <1140956@isep.ipp.pt>
2017-01-05 14:12:28 +0000

    create FlightPlanRegister, SimulationResults

---
+++ src/main/java/lapr/project/model/register/FlightPlanRegister.java
@@ -0,0 +1,40 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package lapr.project.model.register;
+
+import java.util.HashMap;
+import lapr.project.model.Flight;
+import lapr.project.model.FlightPlan;
+import lapr.project.model.network.Segment;
+
+/**
+ *
+ * @author João
+ */
+public class FlightPlanRegister {
+
+    private HashMap<Integer, FlightPlan> mapFlightPlans;
+    private SegmentsRegister regSegment;
+
+    public FlightPlanRegister() {
+        mapFlightPlans = new HashMap<>();
+    }
+
+    public FlightPlan newFlight() {
+        return new FlightPlan();
+    }
+
+    public FlightPlan addFlightPlan(FlightPlan newFlight) {
+        return mapFlightPlans.put(newFlight.getId(), newFlight);
+
+    }
+
+   
+
+    public HashMap<Integer, FlightPlan> getFlightPlansList() {
+        return this.mapFlightPlans;
+    }
+}
