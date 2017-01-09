16a6afe09c1b2fe4ec06754f21cc50749fea3ebe
Tiago Lameirão <1141331@isep.ipp.pt>
2017-01-09 17:21:02 +0000

    ExportHTML

---
+++ src/main/java/lapr/project/utils/ExportHTML.java
@@ -0,0 +1,139 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package lapr.project.utils;
+
+import java.util.Formatter;
+import lapr.project.model.network.AirNetwork;
+import lapr.project.model.register.SegmentsRegister;
+
+/**
+ *
+ * @author Tiago
+ */
+public class ExportHTML {
+    
+    private static AirNetwork m_airnetwork;
+    private static SegmentsRegister segmentsRegister;
+    
+    
+    public ExportHTML(AirNetwork airnetwork){
+        m_airnetwork = airnetwork;
+    }
+    
+    public static void criarFicheiro(Formatter out) throws Exception {
+        out.format("<!DOCTYPE html PUBLIC \"-//W3C//DTD HTML 4.01 Transitional//EN\" \"http://www.w3.org/TR/html4/loose.dtd\">%n<html>%n");
+    }
+    
+    public static void headComTitulo(Formatter out, String nameFile, String titulo) throws Exception {
+        out.format("<head>"
+                + "<meta http-equiv=\"Content-Type\" content=\"text/html; charset=utf-8\">"
+                + "<style type=\"text/css\">"
+                + "table {"
+                + "    width:100%%;"
+                + "}"
+                + "table, th, td {"
+                + "    border: 1px solid black;"
+                + "    border-collapse: collapse;"
+                + "}"
+                + "th, td {"
+                + "    padding: 5px;"
+                + "    text-align: left;"
+                + "}"
+                + "table#t01 tr:nth-child(even) {"
+                + "    background-color: #eee;"
+                + "}"
+                + "table#t01 tr:nth-child(odd) {"
+                + "   background-color:#fff;"
+                + "}"
+                + "table#t01 th    {"
+                + "    background-color: #38090b;"
+                + "    color: white;"
+                + "}"
+                + "</style>"
+                + "<title>%s</title>"
+                + "<link rel=\"icon\" type=\"image/ico\" href=\"http://www.isep.ipp.pt/favicon.ico\">"
+                + "<link href=\"https://www.ipp.pt/logo-ipp.png\">"
+                + "<style type=\"text/css\">"
+                + " p.c5 {color: black; text-align: right}"
+                + " span.c4 {color: black}"
+                + " p.c3 {color: black; text-align: center}"
+                + " p.c2 {text-align: center}"
+                + " div.c1 {text-align: center}"
+                + "</style>"
+                + "</head>", nameFile);
+                abrirBody(out);
+                out.format("<div><img src=\"https://portal.isep.ipp.pt/INTRANET/info/PublicDocumentDownload.aspx?f=357146\" alt=\"Logotipo ISEP\"></div>"
+                + "<div class=\"c1\">"
+                + "<h1>%s</h1>"
+                + "</div>",titulo);
+    }
+    
+    /**
+     * 
+     *
+     * @param out 
+     * @throws Exception
+     */
+    public static void abrirBody(Formatter out) throws Exception {
+        out.format("<body >%n");
+    }
+
+    public static void criarCabeçalho(Formatter out, int x, String titulo, String cor) throws Exception {
+        out.format("<center><h%d>%n<font color=\"%s\" size=6>%s</font></h%d>%n%n</center>", x, cor, titulo, x);
+    }
+
+    public static void criarTable(Formatter out, String caption, String name1, String name2) throws Exception {
+        out.format("<div>"
+                + "<table id=\"t01\">"
+                + "<caption><strong>%s</strong></caption>"
+                + "<tr>"
+                + "<th>%s</th>"
+                + "<th>%s</th>"
+                + "</tr>", caption, name1, name2);
+    }
+
+
+    public static void fecharTable(Formatter out) {
+        out.format("</table>"
+                + "</div>"
+                + "<span class=\"c4\"><br>"
+                + "<br>"
+                + "<br>"
+                + "<br>"
+                + "<br>"
+                + "<br>"
+                + "<br>"
+                + "<br>"
+                + "<br>"
+                + "<br></span>");
+    }
+
+    public static void imagem(Formatter out, String str) {
+        out.format("<div><br>"
+                + "<p class=\"c2\"><img src=\"http://images.slideplayer.com.br/5/1609675/slides/slide_73.jpg\" width=\"250\" height=\"70\" alt=\"Formula Lei de Malus\"></p>"
+                + "<p class=\"c3\">%s</p>"
+                + "</div>"
+                + "<span class=\"c4\"><br>"
+                + "<br></span>", str);
+    }
+
+    /**
+     * 
+     *
+     * @param out .
+     * @throws Exception
+     */
+    public static void fecharFx(Formatter out, String str) throws Exception {
+        out.format("<div style=\"float: left; background-color:whyte; border:1px solid black; color:black; margin:20px; padding:20px;\">"
+                + "<h3>%s</h3>"
+                + "<p>"
+                + "LAPR3<br>"
+                + "</div>"
+                + "</body>"
+                + "</html>", str);
+        out.close();
+    }
+}
