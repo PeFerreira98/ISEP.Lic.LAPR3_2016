e8a595a1bcfc90f8fb22aa738b0c8bb33e1d896b
PeFerreira98 <1140953@isep.ipp.pt>
2017-01-11 15:40:42 +0000

    Add AircraftModelRegisterTest [Unit Testing]

--- src/test/java/lapr/project/model/register/AircraftModelRegisterTest.java
+++ src/test/java/lapr/project/model/register/AircraftModelRegisterTest.java
@@ -20,22 +20,30 @@ import static org.junit.Assert.*;
  * @author zero_
  */
 public class AircraftModelRegisterTest {
-    
+
+    AircraftModel newAircraftModel, newAircraftModel2, newAircraftModel3;
+    AircraftModelRegister instance;
+
     public AircraftModelRegisterTest() {
+        newAircraftModel = new AircraftModel("1A", "AircraftDescription", "Maker", AircraftModel.Type.MIXED, 5, "Motor", AircraftModel.MotorType.TURBOJET, 25, 25, 25, 25, 25, 25, 25, 25, 25, 25, 25, 25, 25, 25, 25, 25, 25);
+        newAircraftModel2 = new AircraftModel("1B", "AircraftDescription", "Maker", AircraftModel.Type.CARGO, 5, "Motor", AircraftModel.MotorType.ELECTRICPROPELER, 25, 25, 25, 25, 25, 25, 25, 25, 25, 25, 25, 25, 25, 25, 25, 25, 25);
+        newAircraftModel3 = new AircraftModel("1C", "AircraftDescription", "Maker", AircraftModel.Type.PASSENGER, 5, "Motor", AircraftModel.MotorType.TURBOPROP, 25, 25, 25, 25, 25, 25, 25, 25, 25, 25, 25, 25, 25, 25, 25, 25, 25);
+        
+        instance = new AircraftModelRegister();
     }
-    
+
     @BeforeClass
     public static void setUpClass() {
     }
-    
+
     @AfterClass
     public static void tearDownClass() {
     }
-    
+
     @Before
     public void setUp() {
     }
-    
+
     @After
     public void tearDown() {
     }
@@ -46,13 +54,10 @@ public class AircraftModelRegisterTest {
     @Test
     public void testAddAircraftModel() {
         System.out.println("addAircraftModel");
-        
-        AircraftModel newAircraftModel = new AircraftModel("1A", "AircraftDescription", "Maker", AircraftModel.Type.MIXED, 5, "Motor", AircraftModel.MotorType.TURBOJET, 25, 25, 25, 25, 25, 25, 25, 25, 25, 25, 25, 25, 25, 25, 25, 25, 25);
-        AircraftModelRegister instance = new AircraftModelRegister();
-        
+
         AircraftModel expResult = new AircraftModel("1A", "AircraftDescription", "Maker", AircraftModel.Type.MIXED, 5, "Motor", AircraftModel.MotorType.TURBOJET, 25, 25, 25, 25, 25, 25, 25, 25, 25, 25, 25, 25, 25, 25, 25, 25, 25);
-        AircraftModel result = instance.addAircraftModel(newAircraftModel);
-        
+        AircraftModel result = this.instance.addAircraftModel(this.newAircraftModel);
+
         assertEquals(expResult, result);
     }
 
@@ -62,15 +67,18 @@ public class AircraftModelRegisterTest {
     @Test
     public void testGetAircraftModelMap() {
         System.out.println("getAircraftModelMap");
+
+        this.instance.addAircraftModel(this.newAircraftModel);
+        this.instance.addAircraftModel(newAircraftModel2);
+        this.instance.addAircraftModel(newAircraftModel3);
         
-        AircraftModel newAircraftModel = new AircraftModel("1A", "AircraftDescription", "Maker", AircraftModel.Type.MIXED, 5, "Motor", AircraftModel.MotorType.TURBOJET, 25, 25, 25, 25, 25, 25, 25, 25, 25, 25, 25, 25, 25, 25, 25, 25, 25);
-        AircraftModelRegister instance = new AircraftModelRegister();
-        
-        instance.addAircraftModel(newAircraftModel);
         Map<String, AircraftModel> expResult = new LinkedHashMap<>();
-        expResult.put(newAircraftModel.getId(), newAircraftModel);
-        Map<String, AircraftModel> result = instance.getAircraftModelMap();
+        expResult.put(this.newAircraftModel.getId(), this.newAircraftModel);
+        expResult.put(this.newAircraftModel2.getId(), this.newAircraftModel2);
+        expResult.put(this.newAircraftModel3.getId(), this.newAircraftModel3);
         
+        Map<String, AircraftModel> result = this.instance.getAircraftModelMap();
+
         assertEquals(expResult, result);
     }
 
@@ -80,18 +88,53 @@ public class AircraftModelRegisterTest {
     @Test
     public void testEquals() {
         System.out.println("equals");
-        
+
         AircraftModel newAircraftModel = new AircraftModel("1A", "AircraftDescription", "Maker", AircraftModel.Type.MIXED, 5, "Motor", AircraftModel.MotorType.TURBOJET, 25, 25, 25, 25, 25, 25, 25, 25, 25, 25, 25, 25, 25, 25, 25, 25, 25);
-        
+
         Object obj = new AircraftModelRegister();
-        AircraftModelRegister instance = new AircraftModelRegister();
-        
         instance.addAircraftModel(newAircraftModel);
-        
+
         boolean expResult = false;
         boolean result = instance.equals(obj);
-        
+
         assertEquals(expResult, result);
     }
-    
+
+    /**
+     * Test of getAircraftModel method, of class AircraftModelRegister.
+     */
+    @Test
+    public void testGetAircraftModel() {
+        System.out.println("getAircraftModel");
+
+        String aircraftModelId = "1A";
+        this.instance.addAircraftModel(newAircraftModel);
+        this.instance.addAircraftModel(newAircraftModel2);
+        this.instance.addAircraftModel(newAircraftModel3);
+
+        AircraftModel expResult = this.newAircraftModel;
+        AircraftModel result = instance.getAircraftModel(aircraftModelId);
+
+        assertEquals(expResult, result);
+    }
+
+    /**
+     * Test of getAircraftsByType method, of class AircraftModelRegister.
+     */
+    @Test
+    public void testGetAircraftsByType() {
+        System.out.println("getAircraftsByType");
+
+        AircraftModel.Type aircraftType = AircraftModel.Type.MIXED;
+        this.instance.addAircraftModel(newAircraftModel);
+        this.instance.addAircraftModel(newAircraftModel2);
+        this.instance.addAircraftModel(newAircraftModel3);
+        
+        Map<String, AircraftModel> expResult = new LinkedHashMap<>();
+        expResult.put(this.newAircraftModel.getId(), this.newAircraftModel);
+        
+        Map<String, AircraftModel> result = instance.getAircraftsByType(aircraftType);
+
+        assertEquals(expResult, result);
+    }
 }
